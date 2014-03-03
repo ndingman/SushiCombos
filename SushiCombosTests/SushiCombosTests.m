32abc70836c77a3369fb333cfb7695b6fb244999
Neil Dingman <neil.dingman@mentormate.com>
2014-03-03 12:22:58 -0600

    Initial Commit

---
+++ SushiCombosTests/SushiCombosTests.m
@@ -0,0 +1,34 @@
+//
+//  SushiCombosTests.m
+//  SushiCombosTests
+//
+//  Created by NEIL DINGMAN on 3/3/14.
+//  Copyright (c) 2014 Neil Dingman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SushiCombosTests : XCTestCase
+
+@end
+
+@implementation SushiCombosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
